596d7cdfe803e5b7a44c415569823050acd7083b
Sufiyan33 <Basu700736@gmail.com>
2023-06-19 19:59:21 +0530

    Adding some java program of string method

---
+++ DataStructure/src/strings/CheckStringContains.java
@@ -0,0 +1,11 @@
+package strings;
+
+public class CheckStringContains {
+
+	public static void main(String[] args) {
+		String str = "IloveJavaCoding";
+		String str1 = "Coding";
+
+		System.out.println("I love Java coding :: " + str.contains(str1));
+	}
+}
---
+++ DataStructure/src/strings/CompareToString.java
@@ -0,0 +1,11 @@
+package strings;
+
+public class CompareToString {
+
+	public static void main(String[] args) {
+		String str = "Sufiyan";
+		String str1 = "College";
+
+		System.out.println(str.compareTo(str1));
+	}
+}

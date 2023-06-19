59af450891f428bdbf1c1491445417cfcba888a5
Sufiyan33 <Basu700736@gmail.com>
2023-06-19 19:34:30 +0530

    Program for concatinating two String

---
+++ DataStructure/src/strings/ConcatTwoString.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class ConcatTwoString {
+
+	public static void main(String[] args) {
+		String s1 = "Sufiyan";
+		String s2 = " I love coding";
+
+		String result = s1.concat(s2);
+		System.out.println("Concat String is :: " + result);
+	}
+}

5b9f1b6ffe4ef1bbc3eab42cc28a04df32c7d8a0
Sufiyan33 <Basu700736@gmail.com>
2023-06-24 22:38:18 +0530

    Program to print 2D array

---
+++ DataStructure/src/arrays/Print2DArray.java
@@ -0,0 +1,22 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class Print2DArray {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 8, 5, 7, 9 } };
+		/*
+		 * for (int row = 0; row < arr.length; row++) { for (int col = 0; col <
+		 * arr[row].length; col++) { arr[row][col] = sc.nextInt(); } }
+		 */
+
+		for (int row = 0; row < arr.length; row++) {
+			for (int col = 0; col < arr[row].length; col++) {
+				System.out.println(arr[row][col] + " ");
+			}
+		}
+	}
+}

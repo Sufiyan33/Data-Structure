e044a46138237a650ac5ed17562f8f0b36dc4eac
Sufiyan33 <Basu700736@gmail.com>
2023-06-27 22:03:22 +0530

    Removed sysout statements and did code changes

--- DataStructure/src/arrays/FindEvenDigitNumbersInArray.java
+++ DataStructure/src/arrays/FindEvenDigitNumbersInArray.java
@@ -5,16 +5,13 @@ public class FindEvenDigitNumbersInArray {
 	public static void main(String[] args) {
 		int[] arr = { 12, 345, 6, 2, 7896, 124, 22 };
 
-		System.out.println("1");
-		System.out.println(findNumber(arr));
+		System.out.println("Even digits are :: " + findNumber(arr));
 	}
 
 	static int findNumber(int[] arr) {
-		System.out.println("2");
 		int count = 0;
 		for (int num : arr) {
 			if (findEven(num)) {
-				System.out.println("5");
 				count++;
 			}
 		}
@@ -22,22 +19,18 @@ public class FindEvenDigitNumbersInArray {
 	}
 
 	static boolean findEven(int num) {
-		System.out.println("3");
 		int numberOfDigits = countDigits(num);
-		System.out.println("5");
 		if (numberOfDigits % 2 == 0) {
-			System.out.println("6");
 			return true;
 		}
 		return false;
 	}
 
 	static int countDigits(int num) {
-		System.out.println("4");
 		int count = 0;
 		while (num > 0) {
 			count++;
-			num = num % 2;
+			num = num / 10;
 		}
 		return count;
 	}

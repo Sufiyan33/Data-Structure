23561244770ca00edd1360f5a674744493db455f
Sufiyan33 <Basu700736@gmail.com>
2023-07-04 11:58:47 +0530

    Program to count pairs with the given sum

---
+++ DataStructure/src/arrays/CountPairsWithGivenSumInArray.java
@@ -0,0 +1,24 @@
+package arrays;
+
+public class CountPairsWithGivenSumInArray {
+
+	/*
+	 * you can also write program using binary search
+	 */
+	public static void main(String[] args) {
+		int arr[] = { 1, 5, 7, -1, 5 };
+		int sum = 6;
+		int result = 0;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] + arr[j] == sum) {
+					result++;
+					// if you want to return index then return i and j;
+					// System.out.println("indexes are :: " + i + "," + j);
+					System.out.println("pairs are :: " + arr[i] + "," + arr[j]);
+				}
+			}
+		}
+		System.out.println("pair is :: " + result);
+	}
+}

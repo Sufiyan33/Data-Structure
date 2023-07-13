3d08343a5d725e2f336626a934286de7f7c86b53
Sufiyan33 <Basu700736@gmail.com>
2023-07-13 22:56:13 +0530

    Find Peak index in a mountain Array leetCode problem

--- DataStructure/src/arrays/PeakIndexinAMountainArray.java
+++ DataStructure/src/arrays/PeakIndexinAMountainArray.java
@@ -2,25 +2,27 @@ package arrays;
 
 public class PeakIndexinAMountainArray {
 	/*
-	 * 852. Peak Index in a Mountain Array leetcode problem.
+	 * 852. Peak Index in a Mountain Array leetCode problem.
 	 */
 	public static void main(String[] args) {
 		// int[] arr = { 0, 1, 0 };
 		// int[] arr = { 0, 2, 1, 0 };
 		int[] arr = { 0, 10, 5, 2 };
+		// int[] arr = { 3, 4, 5, 1 };
 
 		int peak = peakIndexInMountainArray(arr);
 		System.out.println("peak number is :: " + peak);
 	}
 
 	public static int peakIndexInMountainArray(int[] arr) {
+		int result = 0;
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = i + 1; j < arr.length; j++) {
 				if (arr[i] > arr[j]) {
-					return i;
+					result = i;
 				}
 			}
 		}
-		return -1;
+		return result;
 	}
 }

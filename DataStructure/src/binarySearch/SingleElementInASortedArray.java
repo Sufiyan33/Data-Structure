29ea6297828b4674b89f7e88bf3e8eb6f86f3d2e
Sufiyan33 <Basu700736@gmail.com>
2023-07-16 17:49:35 +0530

    Find a only unique number in integer array leetCode problem

---
+++ DataStructure/src/binarySearch/SingleElementInASortedArray.java
@@ -0,0 +1,40 @@
+package binarySearch;
+
+public class SingleElementInASortedArray {
+	/*
+	 * 540. Single Element in a Sorted Array. LeetCode Problem
+	 */
+	/*
+	 * nums = [1,1,2,3,3,4,4,8,8], Output: 2 Input: nums = [3,3,7,7,10,11,11],
+	 * Output: 10
+	 */
+	public static void main(String[] args) {
+		int[] arr = { 1, 1, 2, 3, 3, 4, 4, 8, 8 };
+		int element = findSingleElement(arr);
+		System.out.println("single element is :: " + element);
+	}
+
+	static int findSingleElement(int[] arr) {
+		int start = 0;
+		int end = arr.length - 1;
+
+		while (start < end) {
+			int mid = start + (end - start) / 2;
+
+			// mid is in odd index then subtract 1 to make it even.
+			if (mid % 2 == 1) {
+				mid--;
+			}
+			if (arr[mid] != arr[mid + 1]) {
+				// means we have to search in the left or may be this is the only answer hence
+				// return it.
+				end = mid;
+			} else {
+				// otherwise element must be present in the right side. So start would be start
+				// = mid + 2. to make it even index.
+				start = mid + 2;
+			}
+		}
+		return arr[start];
+	}
+}

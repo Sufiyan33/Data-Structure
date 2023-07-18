eea54874b86d1d07c1e10d62828045cb4ed23c2a
Sufiyan33 <Basu700736@gmail.com>
2023-07-18 10:50:33 +0530

    Added steps that how to merge two arrays

--- DataStructure/src/arrays/MergeTwoSortedArray.java
+++ DataStructure/src/arrays/MergeTwoSortedArray.java
@@ -3,7 +3,11 @@ package arrays;
 import java.util.Arrays;
 
 public class MergeTwoSortedArray {
-
+	/*
+	 * 1- Create a new temp[arr1.length + arr2.length] and take a variable k to
+	 * indicate index of temp array. 2- Simply iterate array one by one and add in
+	 * new temp array 3- If you want to sort it then sort array otherwise return it.
+	 */
 	public static void main(String[] args) {
 		int[] arr1 = { 2, 3, 5, 6, 7, 8 };
 		int[] arr2 = { 4, 9 };

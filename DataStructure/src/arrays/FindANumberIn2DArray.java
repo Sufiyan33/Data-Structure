73908fba63e826eeff256422923df6593b4c84f3
Sufiyan33 <Basu700736@gmail.com>
2023-07-19 12:40:55 +0530

    Search Target in 2D array

---
+++ DataStructure/src/arrays/FindANumberIn2DArray.java
@@ -0,0 +1,43 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class FindANumberIn2DArray {
+	/*
+	 * Here, need to find a number in 2D array. First set lower bound and upper
+	 * bound then try to find out number. Searching in 2D array try to minimize
+	 * search space as minimal as possible.
+	 */
+	// Approach.
+	/*
+	 * Case 1: if arr[row][col == target then you found the number. Case 2: if
+	 * arr[row][col] > target, it means you should ignore that row and column, any
+	 * these number would be greater to target.(col--) Case 3 : if arr[row][col] <
+	 * target, means you should check on next row. Hence update row++;
+	 */
+
+	public static void main(String[] args) {
+		int[][] arr = { { 10, 20, 30, 40 }, { 15, 25, 35, 45 }, { 19, 29, 37, 49 }, { 22, 32, 42, 50 } };
+		int target = 50;
+		int[] findTarget = searchIn2DArray(arr, target);
+		System.out.println(Arrays.toString(findTarget));
+	}
+
+	public static int[] searchIn2DArray(int[][] arr, int target) {
+
+		int row = 0;
+		int col = arr.length - 1;
+
+		while (row < arr.length && col >= 0) {
+
+			if (arr[row][col] == target) {
+				return new int[] { row, col };
+			} else if (arr[row][col] > target) {
+				col--;
+			} else {
+				row++;
+			}
+		}
+		return new int[] { -1, -1 };
+	}
+}

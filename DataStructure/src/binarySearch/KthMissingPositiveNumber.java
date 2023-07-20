da35e23af409902d2a15022a10e1216bd4c99ccb
Sufiyan33 <Basu700736@gmail.com>
2023-07-20 15:42:30 +0530

    Program to find Kth Missing Positive Number LeetCode Problem

---
+++ DataStructure/src/binarySearch/KthMissingPositiveNumber.java
@@ -0,0 +1,33 @@
+package binarySearch;
+
+public class KthMissingPositiveNumber {
+
+	/*
+	 * 1539. Kth Missing Positive Number
+	 */
+
+	public static void main(String[] args) {
+		// int[] arr = { 2, 3, 4, 7, 1 };
+		// int k = 5;
+		int[] arr = { 1, 2, 3, 4 };
+		int k = 2;
+		int missingNumber = findKthPositive(arr, k);
+		System.out.println("missing number is :: " + missingNumber);
+	}
+
+	public static int findKthPositive(int[] arr, int k) {
+		int start = 0;
+		int end = arr.length - 1;
+
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+			if (arr[mid] - mid - 1 < k) {
+				start = mid + 1;
+			} else {
+				end = mid - 1;
+			}
+		}
+
+		return start + k;
+	}
+}

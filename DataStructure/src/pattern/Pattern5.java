547bee95bdad4140839eae5033c657c38afc9364
Sufiyan33 <Basu700736@gmail.com>
2023-07-28 15:48:22 +0530

    Program to print pattern having column size in increasing order.

---
+++ DataStructure/src/pattern/Pattern5.java
@@ -0,0 +1,24 @@
+package pattern;
+
+public class Pattern5 {
+	/*
+	 * Print pattern having number in increasing order.
+	 */
+	/*
+	 * this is very similar to pattern 1 but only different is here instead of *, we
+	 * are printing column number.
+	 */
+	public static void main(String[] args) {
+		int n = 5;
+		pattern5(n);
+	}
+
+	public static void pattern5(int n) {
+		for (int row = 1; row <= n; row++) {
+			for (int col = 1; col <= row; col++) {
+				System.out.print(col + " ");
+			}
+			System.out.println();
+		}
+	}
+}

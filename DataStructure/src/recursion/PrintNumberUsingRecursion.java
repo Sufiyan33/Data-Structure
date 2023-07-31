da158ca840d9db497c495bfbdce55e85639c7400
Sufiyan33 <Basu700736@gmail.com>
2023-07-31 15:21:12 +0530

    Added logic to print number

--- DataStructure/src/recursion/PrintNumberUsingRecursion.java
+++ DataStructure/src/recursion/PrintNumberUsingRecursion.java
@@ -1,5 +1,20 @@
 package recursion;
 
 public class PrintNumberUsingRecursion {
+	/*
+	 * Print number 1 to 5 using recursion.
+	 */
+	public static void main(String[] args) {
+		print(1);
+	}
 
+	static void print(int n) {
+		// Here, define base condition here So that recursive call break here.
+		if (n == 5) {
+			System.out.println(5);
+			return;
+		}
+		System.out.println(n);
+		print(n + 1);
+	}
 }

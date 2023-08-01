5ce1ca331b169f048879b1c8aeab2ad6acae8468
Sufiyan33 <Basu700736@gmail.com>
2023-08-01 15:27:34 +0530

    Program to check weather a number is power of two or not.

---
+++ DataStructure/src/recursion/PowerOfTwo.java
@@ -0,0 +1,22 @@
+package recursion;
+
+public class PowerOfTwo {
+	/*
+	 * 231. Power of Two :: https://leetcode.com/problems/power-of-two/
+	 * 
+	 * Try to solve using recursion.
+	 */
+	public static void main(String[] args) {
+		int n = 20;
+		boolean result = power(n);
+		System.out.println("Power of two := " + result);
+	}
+
+	public static boolean power(int n) {
+		if (n < 1)
+			return false;
+
+		return (n & (n - 1)) == 0 ? true : false;
+
+	}
+}

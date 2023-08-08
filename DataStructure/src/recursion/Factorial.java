133ac886f122a820a8b618623f3dd696fd8b781c
Sufiyan33 <Basu700736@gmail.com>
2023-08-08 15:22:28 +0530

    Program to calculate Factorial of a number using Recursion

---
+++ DataStructure/src/recursion/Factorial.java
@@ -0,0 +1,18 @@
+package recursion;
+
+public class Factorial {
+	/*
+	 * Find out factorial of a number using recursion.
+	 */
+	public static void main(String[] args) {
+		long fact = factorial(20);
+		System.out.println("Factorial of number is  " + fact);
+	}
+
+	public static long factorial(int n) {
+		if (n == 0) {
+			return 1;
+		}
+		return (n * factorial(n - 1));
+	}
+}

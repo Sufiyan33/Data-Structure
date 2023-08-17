71c121efa624dfdeec8a6350c8b78e322a5a99e8
Sufiyan33 <Basu700736@gmail.com>
2023-08-17 12:24:12 +0530

    Program to find Sum of N numbers

---
+++ DataStructure/src/recursion/SumOfNnumber.java
@@ -0,0 +1,21 @@
+package recursion;
+
+public class SumOfNnumber {
+	/*
+	 * Program to find sum of N numbers.
+	 */
+	public static void main(String[] args) {
+		long sum = sum(0);
+		System.out.println("Sum of N number is  " + sum);
+	}
+
+	public static long sum(int n) {
+		if (n == 0) {
+			return 0;
+		}
+		if (n == 1) {
+			return 1;
+		}
+		return (n + sum(n - 1));
+	}
+}

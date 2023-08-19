4084c6d0098f43fecd5aaa90034bfb661bb943c0
Sufiyan33 <Basu700736@gmail.com>
2023-08-19 23:25:05 +0530

    Program to sort array using selection sort by Recursion

---
+++ DataStructure/src/recursion/SelectionSortUsingRecursion.java
@@ -0,0 +1,33 @@
+package recursion;
+
+import java.util.Arrays;
+
+public class SelectionSortUsingRecursion {
+	/*
+	 * Here, we will use selection sort using recursion.
+	 */
+	public static void main(String[] args) {
+		int[] arr = { 5, 4, 3, 2, 1 };
+		selection(arr, arr.length, 0, 0);
+		System.out.println(Arrays.toString(arr));
+	}
+
+	static void selection(int[] arr, int r, int c, int max) {
+		if (r == 0)
+			return;
+
+		if (c < r) {
+			if (arr[c] > arr[max]) {
+				selection(arr, r, c + 1, c);
+			} else {
+				selection(arr, r, c + 1, max);
+			}
+		} else {
+			// swap element.
+			int temp = arr[max];
+			arr[max] = arr[r - 1];
+			arr[r - 1] = temp;
+			selection(arr, r - 1, 0, 0);
+		}
+	}
+}

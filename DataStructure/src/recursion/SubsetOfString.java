99574ccf309f91ac853e2558dc8a5ce709b6d090
Sufiyan33 <Basu700736@gmail.com>
2023-08-22 11:00:43 +0530

    Now Returning list in the same program

--- DataStructure/src/recursion/SubsetOfString.java
+++ DataStructure/src/recursion/SubsetOfString.java
@@ -1,5 +1,7 @@
 package recursion;
 
+import java.util.ArrayList;
+
 public class SubsetOfString {
 	/*
 	 * Find subset of a given String.
@@ -17,6 +19,9 @@ public class SubsetOfString {
 
 	public static void main(String[] args) {
 		subset("", "abc");
+		System.out.println();
+		System.out.println(subsetReturnList("", "123"));
+		System.out.println(subsetReturnList("", "abc"));
 	}
 
 	static void subset(String ans, String str) {
@@ -31,5 +36,28 @@ public class SubsetOfString {
 		subset(ans, str.substring(1));
 		// Taking an elements.
 		subset(ans + ch, str.substring(1));
+	}
+
+	// Now we will return List instead of void.
+	/*
+	 * I already explained this in Program : FindTargetInArrayHavingDuplicateNumber.
+	 * Make sure to check GitHub recursion repo.
+	 */
+
+	static ArrayList<String> subsetReturnList(String ans, String str) {
+		if (str.isEmpty()) {
+			ArrayList<String> list = new ArrayList<>();
+			list.add(ans);
+			return list;
+		}
+
+		char ch = str.charAt(0);
+
+		ArrayList<String> left = subsetReturnList(ans, str.substring(1));
+		ArrayList<String> right = subsetReturnList(ans + ch, str.substring(1));
+
+		// Now combine both result and return.
+		left.addAll(right);
+		return left;
 	}
 }

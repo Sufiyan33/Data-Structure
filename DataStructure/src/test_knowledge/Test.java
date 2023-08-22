28dbd8cf92351df0c2067e495c23404c404f60ad
Sufiyan33 <Basu700736@gmail.com>
2023-08-22 11:23:20 +0530

    Adding here normally testing questions

---
+++ DataStructure/src/test_knowledge/Test.java
@@ -0,0 +1,14 @@
+package test_knowledge;
+
+public class Test {
+	public static void main(String[] args) {
+		short x = 10;
+		x = (short) (x * 5); // you must do type casting.
+		System.out.println(x);
+
+		System.out.println("------Print ASCII value-------");
+
+		char ch = 'a';
+		System.out.println(ch + 0);
+	}
+}

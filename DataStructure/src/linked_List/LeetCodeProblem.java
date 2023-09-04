9e10b82186e0db0af5ea616806f5a4acabab06f5
Sufiyan33 <Basu700736@gmail.com>
2023-09-04 12:24:23 +0530

    92. Reverse Linked List II

--- DataStructure/src/linked_List/LeetCodeProblem.java
+++ DataStructure/src/linked_List/LeetCodeProblem.java
@@ -165,12 +165,42 @@ public class LeetCodeProblem {
 	}
 
 	// Question 5 : 92. Reverse Linked List II
+	public Node reverseBetween(Node head, int left, int right) {
+		if (left == right) {
+			return head;
+		}
 
-	/*
-	 * public Node reverseBetween(Node head, int left, int right) {
-	 * 
-	 * }
-	 */
+		// skip the first left-1 nodes
+		Node current = head;
+		Node prev = null;
+		for (int i = 0; current != null && i < left - 1; i++) {
+			prev = current;
+			current = current.next;
+		}
+
+		Node last = prev;
+		Node newEnd = current;
+
+		// reverse between left and right
+		Node next = current.next;
+		for (int i = 0; current != null && i < right - left + 1; i++) {
+			current.next = prev;
+			prev = current;
+			current = next;
+			if (next != null) {
+				next = next.next;
+			}
+		}
+
+		if (last != null) {
+			last.next = prev;
+		} else {
+			head = prev;
+		}
+
+		newEnd.next = current;
+		return head;
+	}
 
 	// Adding first node or value in node.
 	public void addFirstNode(int value) {

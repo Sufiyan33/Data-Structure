b4e724dbb774bc9943b0242a623cae1bb6d1f675
Sufiyan33 <Basu700736@gmail.com>
2023-09-06 18:04:10 +0530

    Added print statement

--- DataStructure/src/stack/StackMain.java
+++ DataStructure/src/stack/StackMain.java
@@ -2,7 +2,8 @@ package stack;
 
 public class StackMain {
 	public static void main(String[] args) throws StackException {
-		CustomStack stack = new CustomStack();
+
+		CustomStack stack = new CustomStack(5);
 
 		stack.push(3);
 		stack.push(9);
@@ -17,6 +18,36 @@ public class StackMain {
 		System.out.println(stack.pop());
 		System.out.println(stack.pop());
 		System.out.println(stack.pop());
+
+		System.err.println("Operation with Dynamic stack 👌👌👌");
+
+		DynamicStack dStack = new DynamicStack();
+
+		dStack.push(3);
+		dStack.push(9);
+		dStack.push(12);
+		dStack.push(15);
+		dStack.push(18);
+		dStack.push(19);
+		dStack.push(20);
+		dStack.push(12);
+		dStack.push(10);
+		dStack.push(14);
+		dStack.push(21);
+
+		System.err.println("Peek element := " + dStack.peek());
+
+		System.out.println(dStack.pop());
+		System.out.println(dStack.pop());
+		System.out.println(dStack.pop());
+		System.out.println(dStack.pop());
+		System.out.println(dStack.pop());
+		System.out.println(dStack.pop());
+		System.out.println(dStack.pop());
+		System.out.println(dStack.pop());
+		System.out.println(dStack.pop());
+		System.out.println(dStack.pop());
+		System.out.println(dStack.pop());
 
 	}
 }

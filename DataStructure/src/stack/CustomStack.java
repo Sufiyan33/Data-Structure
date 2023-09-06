7794bb2aa66800d790ae3749eab07d1f10022b44
Sufiyan33 <Basu700736@gmail.com>
2023-09-06 12:24:55 +0530

    Changes access modifier

--- DataStructure/src/stack/CustomStack.java
+++ DataStructure/src/stack/CustomStack.java
@@ -70,11 +70,11 @@ public class CustomStack {
 		return data[ptr];
 	}
 
-	private boolean isEmpty() {
+	public boolean isEmpty() {
 		return ptr == -1;
 	}
 
-	private boolean isFull() {
+	public boolean isFull() {
 		return ptr == data.length - 1;// pointer is at last
 	}
 

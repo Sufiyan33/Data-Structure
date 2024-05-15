bbe79abfdbb4cf0a91bafb1ff12329294a8a2edd
Sufiyan33 <Basu700736@gmail.com>
2024-05-15 17:04:30 +0530

    Adding all collection data into TreeSet

--- DataStructure/src/test_knowledge/AddCollection.java
+++ DataStructure/src/test_knowledge/AddCollection.java
@@ -3,6 +3,7 @@ package test_knowledge;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.TreeSet;
 import java.util.Vector;
 
 public class AddCollection {
@@ -10,8 +11,8 @@ public class AddCollection {
 	public static void main(String[] args) {
 		ArrayList<Integer> al = new ArrayList<>();
 		al.add(12);
-		al.add(15);
 		al.add(13);
+		al.add(14);
 		al.add(14);
 
 		System.out.println(al);
@@ -20,12 +21,14 @@ public class AddCollection {
 		v.add(20);
 		v.add(21);
 		v.add(22);
+		v.add(22);
 
 		System.out.println(v);
 
 		LinkedList<Integer> ll = new LinkedList<>(v);
 		ll.add(30);
 		ll.add(31);
+		ll.add(32);
 		ll.add(32);
 
 		System.out.println(ll);
@@ -35,8 +38,16 @@ public class AddCollection {
 		set.add(40);
 		set.add(41);
 		set.add(42);
+		set.add(42);
 
 		System.out.println(set);
+
+		TreeSet<Integer> t = new TreeSet<>(ll);
+		t.add(50);
+		t.add(51);
+		t.add(52);
+
+		System.out.println(t);
 
 	}
 }

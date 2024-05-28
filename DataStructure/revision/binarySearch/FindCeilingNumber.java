42d3783db7388245ae5dab7904f09a674b0b0bbf
Sufiyan33 <basu700736@gmail.com>
2024-05-28 12:49:30 +0530

    Program to find ceiling of a number

---
+++ DataStructure/revision/binarySearch/FindCeilingNumber.java
@@ -0,0 +1,40 @@
+package binarySearch;
+
+public class FindCeilingNumber {
+
+	/*
+	 * Ceiling number is smallest number which is greater than or equal to target element.
+	 */
+	public static void main(String[] args) {
+		int arr []= {1,12,13,14,18,20,21,24};
+		int target = 23;
+		
+		int result  = ceilingNumber(arr, target);
+		System.out.println("Ceiling number at index :: " + result);
+	}
+	
+	static int ceilingNumber(int[] arr, int target) {
+		
+		int start = 0;
+		int end = arr.length-1;
+		
+		if(arr.length == 0)
+			return -1;
+		
+		while(start<= end) {
+			int mid = start + (end-start)/2;
+			
+			if(arr[mid] == target)
+				return arr[mid];
+			
+			if(target > arr[mid]) {
+				start = mid + 1;
+			}else {
+				end = mid - 1;
+			}
+			
+			// if s > e it means condition will break and that is our ceiling number.
+		}
+		return arr[start];
+	}
+}

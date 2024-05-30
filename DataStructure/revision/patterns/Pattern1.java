9aa2f7fad53d4c8879b5e71829332cd0f98f0896
Sufiyan33 <basu700736@gmail.com>
2024-05-30 13:11:48 +0530

    program for pattern 1

---
+++ DataStructure/revision/patterns/Pattern1.java
@@ -0,0 +1,14 @@
+package patterns;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+
+		for (int row = 0; row < 5; row++) {
+			for (int col = 0; col < 5; col++) {
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+	}
+}

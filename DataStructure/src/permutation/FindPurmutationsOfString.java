d2d568efe0c2d30fd214faf483bd33d1759a6843
Sufiyan33 <basu700736@gmail.com>
2024-06-05 19:54:46 +0530

    Add list in argument and all permutations in list

--- DataStructure/src/permutation/FindPurmutationsOfString.java
+++ DataStructure/src/permutation/FindPurmutationsOfString.java
@@ -1,11 +1,16 @@
 package permutation;
 
+import java.util.ArrayList;
+
 public class FindPurmutationsOfString {
 
 	public static void main(String[] args) {
 		// permutations("", "abc");
 		// permutations("", "ab");
-		permutations("", "123");
+		// permutations("", "123");
+
+		ArrayList<String> ans = permutationsReturn("", "123", new ArrayList<String>());
+		System.out.println(ans);
 	}
 
 	static void permutations(String p, String up) {
@@ -30,5 +35,29 @@ public class FindPurmutationsOfString {
 			permutations(first + ch + second, up.substring(1));
 		}
 
+	}
+
+	static ArrayList<String> permutationsReturn(String p, String up, ArrayList<String> list) {
+
+		if (up.isEmpty()) {
+			list.add(p);
+			return list;
+		}
+
+		char ch = up.charAt(0);
+
+		/*
+		 * Now making call as number of process(p) is called. Since process is making
+		 * call => size + 1 times. Hence run loop same time.
+		 */
+
+		for (int i = 0; i < p.length() + 1; i++) {
+
+			String first = p.substring(0, i);
+			String second = p.substring(i, p.length());
+
+			permutationsReturn(first + ch + second, up.substring(1), list);
+		}
+		return list;
 	}
 }

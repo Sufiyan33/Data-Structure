d21a3059061cb7743708b14bbc201dccbee7701b
Sufiyan33 <Basu700736@gmail.com>
2024-06-05 17:53:09 +0530

    Program to skip a word in string using recursion

---
+++ DataStructure/revision/recursion/SkipAWordInString.java
@@ -0,0 +1,13 @@
+package recursion;
+
+public class SkipAWordInString {
+
+	/**
+	 * Skip a word from string using recursion. example bcadapplehd =>bcadbd, Here
+	 * remove apple from string.
+	 */
+
+	public static void main(String[] args) {
+
+	}
+}

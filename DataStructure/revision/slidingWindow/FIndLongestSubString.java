031115796d23ce658142fc9f7e105b098ed1c1f2
Sufiyan33 <basu700736@gmail.com>
2024-06-24 13:48:01 +0530

    Solution with brute force

--- DataStructure/revision/slidingWindow/FIndLongestSubString.java
+++ DataStructure/revision/slidingWindow/FIndLongestSubString.java
@@ -6,23 +6,38 @@ public class FIndLongestSubString {
 	 * Find Longest substring without repeating character
 	 */
 
-	public static int subString(String str) {
-
-		int count = 0;
-
-		for (int i = 0; i < str.length(); i++) {
-			for (int j = i; j < str.length(); j++) {
-				if (str.charAt(i) != str.charAt(j)) {
-					System.out.println(str.charAt(j));
-					count++;
-				}
-			}
-		}
-		return count;
-	}
-
 	public static void main(String[] args) {
-		String str = "cadbzxcab";
+		String str = "cadbzabcd";
 		System.out.println(subString(str));
 	}
+
+	// Brute force approach.
+	public static int subString(String str) {
+		int len = 0;
+		int maxLen = 0;
+		
+		char[] ch = str.toCharArray();
+		final int MAX_SIZE = 256;
+
+		for (int i = 0; i < ch.length; i++) {
+
+			int hash[] = new int[MAX_SIZE];
+			for (int j = i; j < ch.length; j++) {
+
+				if (hash[ch[j]] == 1) {
+					break;
+				}
+
+				len = j - i + 1;
+				maxLen = Math.max(len, maxLen);
+				hash[ch[j]] = 1;
+
+			}
+		}
+		return maxLen;
+	}
+
+	// Optimal Approach
+
+
 }
